bff2725921acedd53de825e5fb19623baeb22f2f
noshaba <noshaba@rocketmail.com>
2015-05-21 12:45:43 +0200

    what am I doing wrong QQ

--- ebvassignment02/ueb02_nosh/sobel/sobel.cc
+++ ebvassignment02/ueb02_nosh/sobel/sobel.cc
@@ -72,7 +72,7 @@ void sobel (Mat_<ushort>& dstImg, const Mat_<uchar>& srcImg)
 			SOBEL1PXFAST(p, pSrc, sys);\
 		\
 		pLine[dstImg.cols - 1] = sobel0;\
-							}\
+	}\
 	\
 	for (p = dstImg.ptr<ushort>(dstImg.rows - 1), pEnd = p + dstImg.cols; p<pEnd; p++) \
 		*p = sobel0;\
@@ -82,21 +82,80 @@ void sobel (Mat_<ushort>& dstImg, const Mat_<uchar>& srcImg)
 
 void sobel1PxFast(ushort* p, const uchar* pSrc, int sys)
 {
-	SOBEL1PXFAST(p, pSrc, sys);
+	// SOBEL1PXFAST(p, pSrc, sys);
+	int sX = (-(int)pSrc[-sys - 1] - ((int)pSrc[-1] << 1) - (int)pSrc[+sys - 1]
+		     + (int)pSrc[-sys + 1] + ((int)pSrc[1] << 1) + (int)pSrc[+sys + 1] + 3) >> 3;
+		
+	int sY = (-(int)pSrc[-sys - 1] - ((int)pSrc[-sys] << 1) - (int)pSrc[-sys + 1]
+			 + (int)pSrc[+sys - 1] + ((int)pSrc[+sys] << 1) + (int)pSrc[+sys + 1] + 3) >> 3; 
+		
+		*p = sobelCode(sX, sY); 
 }
 
 void sobelFast (Mat_<ushort>& dstImg, const Mat_<uchar>& srcImg)
 {
-	SOBELFAST(dstImg, srcImg);
+	// SOBELFAST(dstImg, srcImg);
+	assert(dstImg.size() == srcImg.size()); 
+	
+	ushort *p, *pEnd, *pLine; 
+	const uchar *pSrc; 
+	int sys = srcImg.step[0] / srcImg.step[1];
+
+	for (p = dstImg.ptr<ushort>(0), pEnd = p + dstImg.cols; p < pEnd; p++)
+		*p = sobel0;
+
+	for (int y2 = 1; y2 < dstImg.rows - 1; y2++) {
+
+		pLine = dstImg.ptr<ushort>(y2);
+		pLine[0] = sobel0;
+
+		for (pSrc = srcImg.ptr(y2) + 1, p = pLine + 1, pEnd = p + dstImg.cols - 2; p < pEnd; p++, pSrc++)
+			sobel1PxFast(p, pSrc, sys);
+
+		pLine[dstImg.cols - 1] = sobel0;
+	}
+
+	for (p = dstImg.ptr<ushort>(dstImg.rows - 1), pEnd = p + dstImg.cols; p < pEnd; p++)
+		*p = sobel0;
 }
 
 void sobelFastOpenMP (Mat_<ushort>& dstImg, const Mat_<uchar>& srcImg)
 {
 #ifdef NOOPENMP
-    std::cerr << "Warning: OpenMP not activated." << endl;
+	std::cerr << "Warning: OpenMP not activated." << endl;
 #endif
-    //TODO: implement (1P including changes in sobelFast)
-    // Put the parallelization pragmas except for the parallel section into sobelSIMD
-    // Put the parallel section here so sobelFastOpenMP will run in parallel, 
-    // whereas sobelFast itself will not as it is not inside a parallel section.
+	//TODO: implement (1P including changes in sobelFast)
+	// Put the parallel section here so sobelFastOpenMP will run in parallel, 
+	// whereas sobelFast itself will not as it is not inside a parallel section.
+
+	assert(dstImg.size() == srcImg.size());
+
+	// What am I doing wrong.... Q_Q
+
+	#pragma omp parallel
+	{
+		ushort *p, *pEnd, *pLine;
+		const uchar *pSrc;
+		int sys = srcImg.step[0] / srcImg.step[1];
+
+		#pragma omp for
+		for (p = dstImg.ptr<ushort>(0), pEnd = p + dstImg.cols; p < pEnd; p++)
+			*p = sobel0;
+
+		#pragma omp for
+		for (int y2 = 1; y2 < dstImg.rows - 1; y2++) {
+
+			pLine = dstImg.ptr<ushort>(y2);
+			pLine[0] = sobel0;
+
+			for (pSrc = srcImg.ptr(y2) + 1, p = pLine + 1, pEnd = p + dstImg.cols - 2; p < pEnd; p++, pSrc++)
+				sobel1PxFast(p, pSrc, sys);
+
+			pLine[dstImg.cols - 1] = sobel0;
+		}
+
+		#pragma omp for
+		for (p = dstImg.ptr<ushort>(dstImg.rows - 1), pEnd = p + dstImg.cols; p < pEnd; p++)
+			*p = sobel0;
+	}
 }
