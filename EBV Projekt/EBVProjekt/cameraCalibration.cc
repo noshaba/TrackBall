9f9edee6d69face8fa493e20c8f481195e74935d
noshaba <noshaba@rocketmail.com>
2015-06-18 11:15:53 +0200

    projection implemented

--- EBV Projekt/EBVProjekt/cameraCalibration.cc
+++ EBV Projekt/EBVProjekt/cameraCalibration.cc
@@ -10,14 +10,38 @@
 int CameraCalibration::project (double& x, double& y, const VVector& pInWorld) const
 {
   // TODO: implement (1P)
- return 0;
+	Transform inverseCameraInWorld;
+	inverse(inverseCameraInWorld, cameraInWorld);
+	VVector pInCamera = inverseCameraInWorld * pInWorld;
+
+	// check if behind camera
+	int inside = pInCamera[2] > 0 ? 1 : 0;
+
+	// no K_1 or K_2?!
+	// what to do if z == 0?! division by 0, is this even necessary since point is behind camera?
+	x = centerX + alpha * pInCamera[0] / pInCamera[2];
+	y = centerY + alpha * pInCamera[1] / pInCamera[2];
+
+	// check if inside image
+	inside = isInside(x, y) ? 2 : inside;
+
+	return inside;
 }
 
 
 int CameraCalibration::project (double& x, double& y, double& r, const VVector& pInWorld, double radius) const
 {
   // TODO: implement (1P)
- return 0;
+	int inside = project(x,y,pInWorld);
+
+	double x_r, y_r;
+	project(x_r, y_r, pInWorld + VVector(radius,0,0,0));
+	x_r -= x;
+	y_r -= y;
+
+	r = sqrt(x_r*x_r+y_r*y_r);
+
+	return inside;
 }
 
 
