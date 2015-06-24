77d06069666e8b4ab82262faa3a0d98dba09073e
noshaba <noshaba@rocketmail.com>
2015-06-24 16:46:17 +0200

    dynamic implemented but not sure if correct.....

--- EBV Projekt/EBVProjekt/particleFilter.cc
+++ EBV Projekt/EBVProjekt/particleFilter.cc
@@ -4,7 +4,7 @@
 
 ParticleFilter::Parameters::Parameters ()
 // TODO: choose good parameter (1P)
-:ballRadius(0.02), sigmaVelocity(0), sigmaImage(0), deltaT(1.0 / 25), waitUntilSecondObservation(0.1), nrOfParticles(10)
+:ballRadius(0.02), sigmaVelocity(0.5), sigmaImage(0), deltaT(1.0 / 25), waitUntilSecondObservation(0.1), nrOfParticles(100)
 {
   g[0] = 0;
   g[1] = 0;  
@@ -42,15 +42,19 @@ void ParticleFilter::Particle::observeInit (double x, double y, double r)
 	double pY = Z / alpha * (iY + randomGaussian() - pCenterY);
 
 	if (filter->camera.generate(position, pX, pY, r, radius)) {
-		state = POSITIONDEFINED;
-		timeOfLastObservation = time;
-
-		double dT = time - timeOfLastObservation;
-		if (dT > filter->param.waitUntilSecondObservation) {
-			double vX = 1.0 / dT * (pX - Z / alpha * (iX + randomGaussian() - pCenterX));
-			double vY = 1.0 / dT * (pY - Z / alpha * (iY + randomGaussian() - pCenterY));
-			filter->camera.generate(velocity, vX, vY, r, radius);
-			state = FULLDEFINED;
+		if (state == POSITIONDEFINED){
+			double dT = time - timeOfLastObservation;
+			if (dT > filter->param.waitUntilSecondObservation) {
+				double vX = 1.0 / dT * (pX - Z / alpha * (iX + randomGaussian() - pCenterX));
+				double vY = 1.0 / dT * (pY - Z / alpha * (iY + randomGaussian() - pCenterY));
+				filter->camera.generate(velocity, vX, vY, r, radius);
+				state = FULLDEFINED;
+				timeOfLastObservation = time;
+			}
+		}
+		else {
+			state = POSITIONDEFINED;
+			timeOfLastObservation = time;
 		}
 	}
 	else {
@@ -76,12 +80,21 @@ void ParticleFilter::Particle::observe (double x, double y, double r)
 void ParticleFilter::dynamic ()
 {
     // TODO: implement (included with Particle::dynamic)
+	for (int i = 0; i < particle.size(); ++i) 
+		// TODO: really param.deltaT?!
+		particle[i].dynamic(param.deltaT);
 }
 
 
 void ParticleFilter::Particle::dynamic (double deltaT)
 {
-  // TODO: implement (2P)
+	// TODO: implement (2P)
+	if (state == FULLDEFINED) {
+		VVector ones = { 1, 1, 1, 0 };
+		position += (deltaT * velocity + deltaT * deltaT * filter->param.g * .5).mul(ones); // mul = element wise multiplication
+		velocity += (deltaT * filter->param.g + filter->param.sigmaVelocity * sqrt(deltaT) * randomGaussian() * ones).mul(ones);
+	}
+	time += deltaT;
 }
 
 
